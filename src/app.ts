12117917f39eba1fcee32c47e2de52ffef9b4bea
BinaryDog109 <45409135+BinaryDog109@users.noreply.github.com>
2022-10-02 23:19:55 +0800

    Added cors config so that it could run locally

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,14 @@ import express from 'express'
 import {Express} from "express/ts4.0"
 import config from './config/CONFIG'
 import logger from './util/logger'
+const cors = require('cors')
+
 
 const app:Express = express()
-
+app.use(cors({
+    origin: '*'
+  }));
+app.options('*', cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
